a29df255d471f81fc3bf285f183ecaf11a55ba9e
manish-nx <manish.kumar@successive.tech>
2019-02-08 19:58:17 +0530

    intial setup

---
+++ src/components/homePage/homePage.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <h2>This is the home page</h2>
+    );
+  }
+}
+export default HomePage;
