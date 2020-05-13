13afe84a9b1718e521fc93dc44e4ebd242a41c09
manishsundriyal <msundriyal27@gmail.com>
2020-05-14 01:38:32 +0530

    build fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
     title: `Manish Sundriyal`,
     description: `I'm Manish Kr. Sundriyal, a Full Stack Developer from India. Currently, I work for the Product Team at Successive Technologies. He is a content creator, who mostly shares his content on various platforms like Instagram and Medium`,
     siteUrl: "https://manishsundriyal.com",
+    url: "manishsundriyal.com",
     author: `@manishsundriyal`,
   },
   plugins: [
